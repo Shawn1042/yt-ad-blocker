68e49fecceca645c18e4a770e92d1f7f754cc43b
Shawn1042 <shawnholmes9921@gmail.com>
2023-11-22 17:12:08 -0500

    Refine ad skipping logic and improve robustness of YouTube Ad Skipper
    - Target specific DOM element to reduce observer load and improve efficiency.
    - Introduce a debug logging mechanism, toggleable for easier debugging.
    - Enhance error handling for greater script resilience.
    - Refactor code for improved readability and maintainability.
    - Add detailed comments for better understanding of the script's functionality.

--- script.js
+++ script.js
@@ -1,28 +1,41 @@
 // ==UserScript==
 // @name         Advanced YouTube Ad Skipper
 // @namespace    youtube-ad-skipper
-// @version      2.0
-// @description   skips YouTube ads when in DOM Tree
+// @version      2.1
+// @description  Skips YouTube ads automatically when they appear in the DOM.
 // @author       Shawn
 // @match        *://www.youtube.com/*
 // @grant        none
 // ==/UserScript==
-
 
 (function () {
     'use strict';
 
     class YouTubeAdSkipper {
         constructor() {
+            this.debugMode = false; // Set to true for console logs
             this.init();
         }
 
-        init() {
+        log(message) {
+            if (this.debugMode) {
+                console.log(`YouTube Ad Skipper: ${message}`);
+            }
+        }
 
+        init() {
             this.startObserving();
         }
 
         startObserving() {
+            const targetNode = document.getElementById('page-manager'); // Targeting a more specific element
+
+            if (!targetNode) {
+                this.log('Target node not found. Retrying in 3 seconds.');
+                setTimeout(() => this.init(), 3000);
+                return;
+            }
+
             const observer = new MutationObserver(mutations => {
                 mutations.forEach(mutation => {
                     if (mutation.addedNodes.length) {
@@ -32,7 +45,8 @@
             });
 
             const config = { childList: true, subtree: true };
-            observer.observe(document.body, config);
+            observer.observe(targetNode, config);
+            this.log('Observing for ads...');
         }
 
         checkAndSkipAd() {
@@ -41,26 +55,13 @@
                 
                 if (skipButton && !skipButton.disabled) {
                     skipButton.click();
-                    
-                    console.log("Ad skipped by YouTube Ad Skipper");
+                    this.log('Ad skipped');
                 }
             } catch (error) {
-              
-                console.error("Error in YouTube Ad Skipper:", error);
+                console.error('Error in YouTube Ad Skipper:', error);
             }
         }
     }
-   
+
     new YouTubeAdSkipper();
 })();
-
-// /* 
-// Instructions for use in Tampermonkey:
-// 1. Click the Tampermonkey extension icon in your browser.
-// 2. Choose "Create a new script...".
-// 3. Delete any content that might be pre-populated in the new script template.
-// 4. Copy and paste the entire block of code above into the script editor.
-// 5. Save the script by clicking File -> Save or by pressing Ctrl+S or Cmd+S.
-// 6. Navigate to www.youtube.com to see the script in action. The script will automatically attempt to click the 'Skip Ad' button on YouTube ads.
-// */
-
